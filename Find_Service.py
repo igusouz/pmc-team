1c4a31d3ba2ee422d66835d583a26743b85e812b
Vinicius-Costas <57736519+Vinicius-Costas@users.noreply.github.com>
2024-09-11 14:56:34 -0300

    Find_Service.py

---
+++ Find_Service.py
@@ -0,0 +1,19 @@
+import json
+
+entradas = {
+        "servicos": ["casamento", "revelacao", "noivado", "culto", "alface"]
+}
+
+def verificar(entradas, palavra):
+    servicos = entradas.get("servicos",[])
+
+    if palavra in servicos:
+        return f'Servico "{palavra}" encontrado.'
+    else:
+        return f'Servico "{palavra}" não foi encontrado.'
+    
+result = verificar(entradas, 'casamento')
+print(result)
+
+result = verificar(entradas, 'velorio')
+print(result)
